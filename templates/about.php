e6b2fb272701e07500874054c179a85c410bf0e4
azkanab <azkanabilah@gmail.com>
2018-04-22 21:49:47 +0700

    REALLY FIX

--- templates/about.php
+++ templates/about.php
@@ -3,7 +3,7 @@
 <head>
 	<meta charset="utf-8" />
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<title>About SPAM!</title>
+	<title>SPAM! 2018 | About us</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<!-- Latest compiled and minified CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootswatch/4.1.0/lux/bootstrap.min.css">
